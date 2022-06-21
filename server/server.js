4130264dc8b47986b356b5ad2f898c6cd929d731
diodimi <diodimi14@gmail.com>
2022-06-21 15:35:25 +0300

    Queries added

--- server/server.js
+++ server/server.js
@@ -40,6 +40,50 @@ app.get("/emails/get", (req, res) => {
   });
 });
 
+app.post("/visits/update",(req,res)=>{
+
+})
+
+app.post("/visits/post",(req,res)=>{
+  const email = req.body.email;
+
+  const sqlInsert =
+    "INSERT INTO data(user_email,beggginer_visits,interm_visits,advanced_visits,beginner_passed,intetm_passed,advanced_passed) VALUES (?,0,0,0,0,0,0)";
+  console.log('inserted')
+  db.query(sqlInsert, [ email], (err, result) => {
+    console.log(err);
+})
+})
+
+app.patch("/visits/update",(req,res)=>{
+  const email = req.body.email;
+  const visit = req.body.visit;
+  const sqlSelect = "UPDATE test_app.data SET "+visit+"="+visit+"+1 where user_email=?";
+  db.query(sqlSelect,[ email],  (err, result) => {
+    res.send(result);
+  });
+})
+
+app.get("/visits/get",(req,res)=>{
+  const email = req.body.email;
+  const visit = req.body.visit;
+  const sqlSelect = "SELECT "+visit+" FROM test_app.data where user_email=?";
+  db.query(sqlSelect,[ email],  (err, result) => {
+    res.send(result);
+  });
+})
+
+
+
+app.get("/passed/get",(req,res)=>{
+  const email = req.body.email;
+  const pass = req.body.pass;
+  const sqlSelect = "SELECT "+pass+" FROM test_app.data where user_email=?";
+  db.query(sqlSelect,[ email],  (err, result) => {
+    res.send(result);
+  });
+})
+
 app.post("/accounts/get", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
